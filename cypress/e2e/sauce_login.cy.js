95c2e0bfd29ba9194754cdb18e04cb0fb59f2757
Josue-07 <josuelima875@gmail.com>
2023-07-17 03:45:18 -0300

    test: adicionando testes para a tela de login

--- cypress/e2e/sauce_login.cy.js
+++ cypress/e2e/sauce_login.cy.js
@@ -1,15 +1,46 @@
 /// <reference types="cypress"/>
 
 describe('Tela Login Sauce Demo', () => {
-    it('Validar logon com sucesso', () => {
-        cy.visit('https://www.saucedemo.com/')
-        cy.get('#user-name')
-            .type('standard_user')
+    beforeEach(() => {
+        cy.visit('https://www.saucedemo.com');
 
-        cy.get('#password')
-            .type('secret_sauce')
-
-        cy.get('#login-button')
-            .click()
     });
+    afterEach(() => {
+        cy.screenshot()
+    });
+
+    it('CT001 - Validar logon com sucesso', () => {
+
+        // Preencher campos de login
+        cy.get('#user-name').type('standard_user');
+        cy.get('#password').type('secret_sauce');
+
+        // Enviar formulário de login
+        cy.get('.btn_action').click();
+
+        // Verificar se o logon foi realizado com sucesso
+        cy.url().should('include', '/inventory.html');
+        cy.get('.title').should('contain', 'Products');
+    });
+
+    const usernames = ['standard_user', 'problem_user', 'performance_glitch_user']
+    let cont = 2
+    usernames.forEach(user => {
+        it(`CT00${cont} Validar logon bem sucedido com o username -> "${user}"`, () => {
+            cy.get('#user-name').type(`${user}`);
+            cy.get('#password').type('secret_sauce');
+
+            // Enviar formulário de login
+            cy.get('.btn_action').click();
+
+            // Verificar se o logon foi realizado com sucesso
+            cy.url().should('include', '/inventory.html');
+            cy.get('.title').should('contain', 'Products');
+        });
+        cont++
+
+    })
+
 });
+
+
