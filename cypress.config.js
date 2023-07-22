04193a67a21315e7e72ac2dc18ba80540e308d60
Josue-07 <josuelima875@gmail.com>
2023-07-22 18:47:25 -0300

    feat: subindo nova versão do cypress

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  "pageLoadTimeout": 10000,
   "chromeWebSecurity": false,
   e2e: {
     setupNodeEvents(on, config) {
